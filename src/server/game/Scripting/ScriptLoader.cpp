7553738f5e7711822bb0624661c69897dd329c4c
Bengelbaer <bengelbaer@gmx.de>
2013-09-02 11:05:06 +0200

    Adding Transmogrification to the Core Signed-off-by: Bengelbaer <bengelbaer@gmx.de>

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1391,6 +1391,7 @@ void AddBattlegroundScripts()
 #ifdef SCRIPTS
 /* This is where custom scripts' loading functions should be declared. */
 
+void AddSC_CPWS_Transmogrification();
 #endif
 
 void AddCustomScripts()
@@ -1398,5 +1399,6 @@ void AddCustomScripts()
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
 
+    AddSC_CPWS_Transmogrification();
 #endif
 }
